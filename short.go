1a2d77d6b85e128fdc7b7b039ee9888b3bd4e6ac
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-16 20:53:18 +0800

    adding godoc

--- short.go
+++ short.go
@@ -1,3 +1,17 @@
+/*
+Package short is a simple web based api. It will produce shortest hash possible for a url. It will use longer hashes only in case of collision.
+short has has two entries:
+
+/post which accepts a url and returns a short version of it.
+
+	{"url": "https://example.org"}
+
+/TOKEN which based on request headers will return a json reply or will redirect the agent to the original url.
+
+short uses redis for its backned storage. You can set the redis url in command or in code.
+
+	$ REDISURL="redis://:PASSWORD@pub-zone.redislabs.com:12919/0"
+*/
 package short
 
 import (
@@ -13,9 +27,11 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-//Site is good
+//Site defines main attributes of shortener service.
 type Site struct {
-	Host     string
+	// Host will define the prefix to be used for generated short token for redirects.
+	Host string
+	// RedisURL defines the redis instance to use. If it is empty REDISURL environment variable will be used.
 	RedisURL string
 }
 
@@ -70,7 +86,17 @@ type shortRequest struct {
 	URL string
 }
 
-// Post Only accepts JSON
+/*
+Post Only accepts json data in the following format
+
+	{"url": "https://example.org"}
+
+return results is always in the following format:
+
+	{"short":"https://short.me/ab", "error":""}
+
+If there was any error short will be empty and error message will show.
+*/
 func (site Site) Post(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/javascript")
 	decoder := json.NewDecoder(r.Body)
@@ -89,7 +115,13 @@ func (site Site) Post(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Redirect Only accepts JSON
+/*
+Redirect will either return a json value in the following format:
+
+ 	{"url":"https://example.org", "error":""}
+
+or it will do a 301, http redirect. Return action is based on headers.
+*/
 func (site Site) Redirect(w http.ResponseWriter, r *http.Request) {
 	redisdb := site.redisdb()
 	defer redisdb.Close()
