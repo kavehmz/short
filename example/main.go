1cebc37f294cf15755714e779ed1c3d1907dde26
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-03-02 22:50:39 +0800

    addnig limit for number of serving clients

--- example/main.go
+++ example/main.go
@@ -13,5 +13,26 @@ func main() {
 	site := short.Site{Host: "https://short.kaveh.me/"}
 	http.HandleFunc("/", site.Redirect)
 	http.HandleFunc("/post", site.Post)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// If pool is full, connections will wait.
+	// This is not a good pattern for high scale sites.
+	// This only helps if http connection as a resource is cheaper
+	// than underlying resources like db connetion,...
+	maxServingClients := 2
+	maxClientsPool := make(chan bool, maxServingClients)
+
+	server := &http.Server{
+		Addr:    ":8080"
+		Handler: nil,
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			switch state {
+			case http.StateNew:
+				maxClientsPool <- true
+			case http.StateClosed, http.StateHijacked:
+				<-maxClientsPool
+
+			}
+		},
+	}
+	log.Fatal(server.ListenAndServe())
 }
